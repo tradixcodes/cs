c0f7ee4f551c42f8f7c9a4894432bac3cc0725ea
Tradix <tradix2021@gmail.com>
2023-10-13 13:28:17 +0300

    had to learn what i was doing wrong

--- 2.1/ICS_2201-OOP_II/bmi_calculator.java
+++ 2.1/ICS_2201-OOP_II/bmi_calculator.java
@@ -1,33 +1,32 @@
 import java.util.Scanner;
 
 public class bmi_calculator {
-    public static void calculateBMI(double h, double w) {
+    public static void calculateBMI(double h, double w){
         double bmi = (w / (h * h));
-
-        System.out.printf("Your BMI is: %.2f%n", bmi);
-
-        if (bmi < 18.5) {
+        
+        System.out.printf("Your BMI is : %f\n", bmi);
+        
+        if (bmi < 18.5){
             System.out.println("You are underweight!");
-        } else if (bmi <= 24.9) {
-            System.out.println("You have a normal weight.");
-        } else if (bmi <= 29.9) {
+        }else if (bmi <= 24.9){
+            System.out.println("You have normal weight");
+        }else if (bmi <= 29.9){
             System.out.println("You are overweight!");
-        } else {
+        }else{
             System.out.println("You are obese!");
         }
     }
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         double weight, height;
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter your weight in kilograms: ");
         weight = scanner.nextDouble();
-
-        System.out.print("Enter your height in meters: ");
-        height = scanner.nextDouble();
-
+        
+        System.out.print("Enter your height in metres: ");
+        height = scanner.nextDouble();      
+        
         scanner.close();
 
         calculateBMI(height, weight);
