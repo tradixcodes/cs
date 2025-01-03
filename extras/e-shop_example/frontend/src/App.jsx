b089e3177423f0c2f522cdf1b82e15da7d9b03c7
Tradix and Jesus <tradix2021@gmail.com>
2025-01-03 20:40:30 +0300

    Category_2_added

--- extras/e-shop_example/frontend/src/App.jsx
+++ extras/e-shop_example/frontend/src/App.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Category from "./components/Category/Category";
+import Category2 from "./components/Category/Category2";
+import Services from "./components/Services/Services";  
 
 const App = () => {
   return (
-    <div>
+    <div className="bg-white dark:bg-gray-900 dark:text-white 
+    duration-200 overflow-hidden">
       <Navbar/>
       <Hero/>
       <Category/>
+      <Category2/>
+      <Services/>
     </div>
   )
 }
--- extras/e-shop_example/frontend/src/components/Category/Category2.jsx
+++ extras/e-shop_example/frontend/src/components/Category/Category2.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Image1 from '../../assets/category/earphone.png'
-import Image2 from '../../assets/category/watch.png'
-import Image3 from '../../assets/category/macbook.png'
+import Image1 from '../../assets/category/gaming.png'
+import Image2 from '../../assets/category/vr.png'
+import Image3 from '../../assets/category/speaker.png'
 import Button from '../Shared/Button.jsx'
 
 const Category = () => {
@@ -9,15 +9,15 @@ const Category = () => {
     <div className='py-8'>
         <div className='container'>
             <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8'>
-                {/* first column */}
-                <div className='py-10 pl-5 bg-gradient-to-br from-black/90
-                 to-black/70 text-white rounded-3xl relative h-[320px]
+                {/* third column (long)*/}
+                <div className='col-span-2 py-10 pl-5 bg-gradient-to-br from-gray-400/90
+                 to-gray-100 text-white rounded-3xl relative h-[320px]
                  flex items-end'>
                     <div>
                         <div className='mb-4'>
-                            <p className='mb-[2px] text-gray-400'>Enjoy</p>
+                            <p className='mb-[2px] text-white'>Enjoy</p>
                             <p className='text-2xl font-semibold mb-[2px]'>With</p>
-                            <p className='text-4xl xl:text-5xl font-bold opacity-20 mb-2'>Earphones</p>
+                            <p className='text-4xl xl:text-5xl font-bold opacity-60 mb-2'>Consoles</p>
                             <Button
                             text="Browse"
                             bgColor={"bg-primary"}
@@ -26,45 +26,45 @@ const Category = () => {
                         </div>
                     </div>
                     <img src={Image1} alt=""
-                    className='w-[320px] absolute bottom-0'/>
+                    className='w-[350px] absolute top-1/2 -translate-y-1/2 -right-0'/>
                 </div>
-                {/* second column */}
-                <div className='py-10 pl-5 bg-gradient-to-br from-brandYellow
-                 to-brandYellow/90 text-white rounded-3xl relative h-[320px]
-                 flex items-end'>
+                {/* first column */}
+                <div className='py-10 pl-5 bg-gradient-to-br from-brandGreen/90
+                 to-brandGreen/90 text-white rounded-3xl relative h-[320px]
+                 flex items-start'>
                     <div>
                         <div className='mb-4'>
                             <p className='mb-[2px] text-white'>Enjoy</p>
                             <p className='text-2xl font-semibold mb-[2px]'>With</p>
-                            <p className='text-4xl xl:text-5xl font-bold opacity-40 mb-2'>Gadgets</p>
+                            <p className='text-4xl xl:text-5xl font-bold opacity-20 mb-2'>Oculi</p>
                             <Button
                             text="Browse"
                             bgColor={"bg-white"}
-                            textColor={"text-brandYellow"}
+                            textColor={"text-brandGreen"}
                             />
                         </div>
                     </div>
                     <img src={Image2} alt=""
-                    className='w-[320px] absolute -right-4 lg:top-[40px]'/>
+                    className='w-[320px] absolute bottom-0'/>
                 </div>
-                {/* third column (long) */}
-                <div className='col-span-2 py-10 pl-5 bg-gradient-to-br from-brandRed
-                 to-brandRed/90 text-white rounded-3xl relative h-[320px]
-                 flex items-end'>
+                {/* second column */}
+                <div className='py-10 pl-5 bg-gradient-to-br from-brandBlue
+                 to-brandBlue/90 text-white rounded-3xl relative h-[320px]
+                 flex items-start'>
                     <div>
                         <div className='mb-4'>
                             <p className='mb-[2px] text-white'>Enjoy</p>
                             <p className='text-2xl font-semibold mb-[2px]'>With</p>
-                            <p className='text-4xl xl:text-5xl font-bold opacity-60 mb-2'>Laptops</p>
+                            <p className='text-4xl xl:text-5xl font-bold opacity-40 mb-2'>Speakers</p>
                             <Button
                             text="Browse"
                             bgColor={"bg-white"}
-                            textColor={"text-brandRed"}
+                            textColor={"text-brandBlue"}
                             />
                         </div>
                     </div>
                     <img src={Image3} alt=""
-                    className='w-[350px] absolute top-1/2 -translate-y-1/2 -right-0'/>
+                    className='w-[200px] absolute bottom-0 right-0'/>
                 </div>
             </div>
         </div>
